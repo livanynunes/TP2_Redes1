ca6f609249d459c1ca7911f8ac7ce4249611526f
Livany <40469622+livanynunes@users.noreply.github.com>
2019-10-30 23:02:41 -0300

    Finalizado!
    - Atualiazação do horário dos clientes ocorre após 5 segundos.
    - Tratamento de erros
    - Comentários sobre o código.

--- clients.py
+++ clients.py
@@ -1,31 +1,41 @@
 import socket
 import sys
+
+#biblioteca par capturar hora e data
 from calendar import*;
 from time import*;
+
+# biblioteca para utilizar função de controle de tempo
+import time 
 
 #Importando biblioteca de threads
 from _thread import *
 
-host = '192.168.0.105' ## mudar pro host da maquina do cliente
 port = 8888
+host = '192.168.0.101' 		# host da maquina do cliente
+#host = '192.168.1.30' 		# muda de acordo com o cliente,
+							# para saber, usar 'ipconfig' 
 
+# cria socket para conexão
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 c.connect((host,port))
-welcome_message = c.recv(1024)
-print(welcome_message.decode())
 
-while True:
+welcome_message = c.recv(1024)	# recebendo mensagem de boas vindas do servidor
+print(welcome_message.decode())	# imprimeindo mensagem de boas vindas
 
-	time_recv = c.recv(1024)
+# laço de conexão, para sair digite Ctrl-C
+while True:	
+
+	# recebendo data e hora do servidor, e imprimindo
+	time_recv = c.recv(1024)	
 	print('As infos de data e hora : ' + time_recv.decode())
 
-	continuar = input('Deseja continuar [y/n]? : ')
-	if continuar =='y' or continuar == '':
-		c.send("Atualizou a hora!".encode());
-		continue
-	else :
-		break
+	# envia mensagem ao servidor que hora do cliente foi atualizada!
+	# serve para confirmar que o cliente ainda está conectado
+	c.send("Atualizou a hora!".encode());
 
-#fechar conexao
+	# atualiza o horário a cada X segundos, para testes bom deixar 5seg 
+	time.sleep(5) # para o exercicio utilizamos 60segundos
+	
+#fechando conexao
 c.close()
--- server.py
+++ server.py
@@ -1,55 +1,62 @@
 import socket
 import sys
+
+#biblioteca par capturar hora e data
 from calendar import*;
 from time import*;
 
 #Importando biblioteca de threads
 from _thread import *
 
-host = '' # ??
+host = '' 
 port = 8888
 
+# cria socket para conexão
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("Socket criado!")
 
-try:
+
+try:						# Verifica se o socket está associado ao endereço local
 	s.bind((host,port))
-except socket.error:
+except socket.error:		# caso contrário, aborta!
 	print("Bind falhou!")
 	sys.exit()
 
 print("Conexão criada!")
 
-s.listen(10)
+s.listen(10)				# permite até 10 clientes
 
 print("Servidor está escutando...")
 
 def clientthread(conn,addr):
+	# enviando mensagem de boas vindas ao cliente qque se conectar
 	welcome_message = ("Bem vindo ao servidor, cliente "  + str(addr[0]) + "\n")
 	conn.send(welcome_message.encode())
 
-	time_message = asctime(localtime(time())) #teste 
+	# enviando informações de data e hora aos clientes conectados
+	time_message = asctime(localtime(time()))  
+	conn.send(time_message.encode()) 
 
-	conn.send(time_message.encode()) #teste
-
+	# laço para conexão
 	while True:
-		data = conn.recv(1024)
-		reply = "Cliente "+ str(addr[0]) +": "+ data.decode()
-		if not data:
+		try:						# enquanto receber dados sobre conexao
+			data = conn.recv(1024)	# recebe mensagem de 'atualizou a hora'
+			reply = "Cliente "+ str(addr[0]) +": "+ data.decode()
+		except Exception as e:		# caso contrário, aborta!
 			print("Bye bye, " + str(addr[0]) + "\n")
 			break;
-		print(reply)
-		time_message = asctime(localtime(time()))
-		conn.sendall(time_message.encode()) #inunda rede
+		print(reply)				# imprime mensagem de 'atualizou a hora'
+
+		time_message = asctime(localtime(time()))	# Atualiza hora e data
+		conn.sendall(time_message.encode()) 		# Inunda rede com infos atualizadas
 	conn.close()
 
-#######################
-
-while 1:
-	conn, addr = s.accept()
-	print("Conectado com "+ addr[0] + ":" + str(addr[1]))
-	start_new_thread(clientthread,(conn,addr))
+# laço que cria conexão com mais de 1 cliente, utilizando threads
+while 1:	
+	conn, addr = s.accept() 								# hand shake, pois nosso socket é tcp
+	print("Conectado com "+ addr[0] + ":" + str(addr[1]))	# imprime informações da máqquina conectada
+	start_new_thread(clientthread,(conn,addr))				# inicia conexão
 
 
 s.close()
